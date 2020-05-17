ebebdf7341d3d7ca27b16a6d2be059ccc2a14a77
Aleksei Lebedev <allebdev@gmail.com>
2020-05-17 18:24:09 +0300

    Move on tbot library

--- src/input/telegram.rs
+++ src/input/telegram.rs
@@ -1,11 +1,8 @@
-use std::time::Duration;
-use std::{env, io};
+use std::{convert::Infallible, env, io, sync::Arc};
 
 use log::*;
-use telegram_bot::{
-    Api, CanReplySendMessage, Error, GetUpdates, MessageKind, SendMessage, Update, UpdateKind,
-};
-use tokio::stream::StreamExt;
+use tbot::{contexts::fields::Text, errors, prelude::*, types::update, Bot};
+use tokio::{select, sync::mpsc, sync::RwLock, time};
 
 use async_trait::async_trait;
 
@@ -14,13 +11,8 @@ use crate::input::InputHandler;
 
 pub struct TelegramInputHandler {
     parser: RawMessageParser,
-    api: Api,
-    mode: StartMode,
-}
-
-enum StartMode {
-    ReadOnly,
-    Polling,
+    timeout: time::Duration,
+    tx: Option<mpsc::Sender<update::Id>>,
 }
 
 #[async_trait(? Send)]
@@ -29,86 +21,107 @@ impl InputHandler for TelegramInputHandler {
         "Telegram"
     }
 
-    async fn start(self) -> io::Result<()> {
-        let result = match self.mode {
-            StartMode::ReadOnly => self.read_updates().await,
-            StartMode::Polling => self.poll_updates().await,
+    async fn start(mut self) -> io::Result<()> {
+        let timeout = self.timeout;
+        info!("Start polling updates (timeout: {} sec)", timeout.as_secs());
+        let (tx, mut rx) = mpsc::channel(10);
+        self.tx.replace(tx);
+
+        let stop = async {
+            loop {
+                if let Ok(id) = time::timeout(timeout, rx.recv()).await {
+                    if let Some(id) = id {
+                        debug!("Update {} handled", id.0);
+                    } else {
+                        info!("Sender is closed, no more updates will come");
+                        break;
+                    }
+                } else {
+                    info!("Timeout");
+                    break;
+                }
+            }
         };
-        match result {
-            Ok(_) => Ok(()),
-            Err(err) => Err(io::Error::new(io::ErrorKind::Other, err)),
+
+        let polling = self.poll_updates();
+
+        select! {
+            _ = stop => {
+                info!("Stop polling updates");
+                Ok(())
+            },
+            result = polling => match result {
+                Ok(_) => Ok(()),
+                Err(errors::PollingSetup::DeleteWebhook(err)) => {
+                    Err(io::Error::new(io::ErrorKind::Other, err))
+                },
+                Err(errors::PollingSetup::DeleteWebhookTimeout(_)) => {
+                    Err(io::Error::new(io::ErrorKind::Other, "Timeout"))
+                },
+            },
         }
     }
 }
 
 impl TelegramInputHandler {
     pub fn new(parser: RawMessageParser) -> Self {
-        let token = env::var("BOT_TOKEN").expect("BOT_TOKEN not set");
-        let mode = env::var("BOT_READONLY").map_or(StartMode::Polling, |_| StartMode::ReadOnly);
+        let timeout =
+            env::var("BOT_TIMEOUT").map_or(5, |v| v.parse().expect("BOT_TIMEOUT must be a number"));
         TelegramInputHandler {
             parser,
-            api: Api::new(token),
-            mode,
+            timeout: time::Duration::from_secs(timeout),
+            tx: None,
         }
     }
 
-    async fn read_updates(&self) -> Result<(), Error> {
-        debug!("Read updates only");
-        let mut updates = GetUpdates::new();
-        let request = updates.timeout(4);
-        let result = self
-            .api
-            .send_timeout(request, Duration::from_secs(5))
-            .await?;
-        if let Some(ref updates) = result {
-            for update in updates {
-                if let Some(response) = self.process_update(&update) {
-                    debug!("Unsent response: {:?}", response)
-                }
+    async fn poll_updates(self) -> Result<Infallible, errors::PollingSetup> {
+        let timeout = self.timeout.as_secs() + 1;
+        let mut bot = Bot::from_env("BOT_TOKEN").stateful_event_loop(RwLock::new(self));
+
+        bot.text(|ctx, this| async move {
+            this.read().await.process_text(ctx, false).await;
+        });
+
+        bot.edited_text(|ctx, this| async move {
+            this.read().await.process_text(ctx, true).await;
+        });
+
+        bot.after_update(|upd, this| async move {
+            let mut this = this.write().await;
+            let sender = this.tx.as_mut().unwrap();
+            sender.send(upd.update_id).await.unwrap();
+        });
+
+        bot.polling().timeout(timeout).start().await
+    }
+
+    async fn process_text<'s>(&self, ctx: Arc<impl Text>, edited: bool) {
+        let username = ctx
+            .from()
+            .and_then(|user| user.username.as_deref())
+            .unwrap_or_default();
+        let value = &ctx.text().value;
+
+        debug!(
+            "Message #{} from {}: '{}'",
+            ctx.message_id(),
+            username,
+            value
+        );
+
+        let output = self.parser.handle_message(Input {
+            id: ctx.message_id().0 as i64,
+            user: username.to_owned(),
+            text: value.clone(),
+            is_new: !edited,
+        });
+
+        if let Some(Output { text }) = output {
+            debug!("Reply to message #{}: {:?}", ctx.message_id(), text);
+            let result = ctx.send_message_in_reply(&text).call().await;
+            if let Err(err) = result {
+                error!("Error on reply to message #{}: {}", ctx.message_id(), err);
             }
-        }
-        Ok(())
-    }
-
-    async fn poll_updates(&self) -> Result<(), Error> {
-        debug!("Polling updates");
-        let stream = self.api.stream();
-        let mut stream = StreamExt::timeout(stream, Duration::from_secs(5));
-        while let Some(Ok(update)) = stream.next().await {
-            let update = &update?;
-            if let Some(response) = self.process_update(update) {
-                self.api.send(response).await?;
-            }
-        }
-        Ok(())
-    }
-
-    fn process_update<'s>(&self, update: &Update) -> Option<SendMessage<'s>> {
-        match &update.kind {
-            UpdateKind::Message(message) | UpdateKind::EditedMessage(message) => match message.kind
-            {
-                MessageKind::Text { ref data, .. } => {
-                    info!(
-                        "Message #{} from {}: '{}'",
-                        message.id, message.from.first_name, data
-                    );
-                    let username = message.from.username.as_deref().unwrap_or_default();
-                    let output = self.parser.handle_message(Input {
-                        id: message.id.into(),
-                        user: username.to_owned(),
-                        text: data.to_owned(),
-                        is_new: message.edit_date.is_none(),
-                    });
-                    if let Some(Output { text }) = output {
-                        info!("Reply to message #{}: '{:?}'", message.id, text);
-                        Some(message.text_reply(text))
-                    } else {
-                        None
-                    }
-                }
-                _ => None,
-            },
-            _ => None,
         }
     }
 }
